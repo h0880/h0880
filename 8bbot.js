139115a0cde9796f0ceaab07ed9d570ebd82236c
Stuart Hobson <h0880@aol.com>
2021-07-31 20:21:02 +0100

    Update 8bbot.js

--- 8bbot.js
+++ 8bbot.js
@@ -7,7 +7,7 @@
 require('dotenv').config(); 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
+client.login(process.env.BOTTOKEN);
 client.on("ready", () => {
   console.log("I am ready!");
 });
